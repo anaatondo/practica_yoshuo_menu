05ff8a1717adfb011d373e8f210ccf69cecee8ec
Administrador <Administrador@ana-fc06cc56197.lan>
2013-03-23 22:32:29 +0100

    programas

---
+++ tarea/src/NotHelloWorldFrame.java
@@ -0,0 +1,14 @@
+
+public class NotHelloWorldFrame {
+
+	public void setVisible(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setDefaultCloseOperation(int exitOnClose) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
