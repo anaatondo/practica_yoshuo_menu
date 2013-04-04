e6aeb7e49d06d7cf97c51875e54e368012b38b08
Administrador <Administrador@169.254.75.101>
2013-04-04 20:24:50 +0200

    practica yoshuo de menu

---
+++ Pract_yoshuo_comobox/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Ventana v = new Ventana();
+		//ClaseComponentes c = new ClaseComponentes();
+		//ClaseComponentePosicion c = new ClaseComponentePosicion(500,400,"Demo Elemento Posicion");
+		//c.setVisible(true);
+		DemoMenu ds = new DemoMenu(500,400,"Menu");
+		ds.setVisible(true);
+	}
+
+
+}
